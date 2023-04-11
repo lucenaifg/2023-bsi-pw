67f6ca5fd004feea569cd0bf152af4e1bd6805b3
Daniel Lucena <cto@mindify.net>
2023-04-10 21:37:06 -0300

    Comunicação entre front-end e back-end utilizando Fetch API.

---
+++ src/main/java/br/edu/ifg/luziania/RetornoAutenticacao.java
@@ -0,0 +1,14 @@
+package br.edu.ifg.luziania;
+
+public class RetornoAutenticacao {
+
+    private String retorno;
+
+    public String getRetorno() {
+        return retorno;
+    }
+
+    public void setRetorno(String retorno) {
+        this.retorno = retorno;
+    }
+}
--- src/main/resources/META-INF/resources/js/login.js
+++ src/main/resources/META-INF/resources/js/login.js
@@ -1,1 +1,38 @@
-alert("Carregou!");
+//Validar formulário
+function validar_formulario(){
+    return document.getElementById("email").value !== '' && document.getElementById("senha").value !== '';
+}
+
+//Criar uma requisição
+function criarRequisicao(email, senha){
+    return new Request("http://localhost:8080/autenticar", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "email": email,
+            "senha": senha
+        }),
+    });
+}
+
+//Solicitar autenticação no servidor
+function autenticar(){
+    if (validar_formulario()){
+        var requisicao = criarRequisicao(document.getElementById("email").value,document.getElementById("senha").value);
+        fetch(requisicao)
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    throw new Error("Ocorreu algum erro no servidor!");
+                }
+            })
+            .then(json => {
+                console.log(JSON.stringify(json))
+            });
+    } else
+        alert('Os campos e-mail e senha são obrigatórios! Verifique o formulário.')
+}
